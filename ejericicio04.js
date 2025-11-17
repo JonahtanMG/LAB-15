c5e52b1e5444cac6918e77bd587feb4fbd368017
JonahtanMG <jmamanigut@unsa.edu.pe>
2025-11-17 10:09:44 -0500

    Delete ejericicio04.js

--- ejericicio04.js
+++
@@ -1,8 +0,0 @@
-const lista = document.querySelectorAll("li");
-for(elemento of lista){
-    elemento.classList.add('resaltado');
-    
-    if(elemento.classList.contains('oculto')){
-        elemento.classList.remove('oculto');
-    }
-};
